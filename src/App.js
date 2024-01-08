d65397f2ea63e57b9bb0872258851f68ad675123
Nathan Brunnett <natebrunnett@gmail.com>
2024-01-08 06:26:33 +0100

    rework

--- src/App.js
+++ src/App.js
@@ -1,49 +1,53 @@
 import "./App.css";
 
-let pfp = require('./images/pfp.jpg');
+let pfp = require('./media/images/pfp.jpg');
 
 function App() {
   return (
     <>
     <div class="header">
-      <h1>Nathan Brunnett</h1>
+      <h2>Nathaniel Brunnett</h2>
       <p>Programming Enthusiast</p>
     </div>
-
+    
     <div class="topnav">
-      <a href="http://localhost:3000/">Home</a>
+        <a href="http://localhost:3000/">Home</a>
     </div>
 
-    <div class="row">
-      
-      <div class="leftcolumn">
-        <div class="card">
-          <h2>Barcelona Coding School</h2>
-          <h5>October 7, 2023 - December 23, 2023</h5>
-          <div class="imageOfSchoolLogo"></div>
-          <p>Going to do some serious coding for now!</p>
+    <div class="wrapper">
+
+      <div class="row">
+        
+        <div class="leftcolumn">
+          <div class="card">
+            <h2>Barcelona Coding School</h2>
+            <h5>October 7, 2023 - December 23, 2023</h5>
+            <div class="imageOfSchoolLogo"></div>
+            <p>Going to do some serious coding for now!</p>
+          </div>
         </div>
+
+        <div class="rightcolumn">
+          <div class="card">
+            <h2>About Me</h2>
+            <img src={pfp} alt="profile picture" />
+            <p>I hope to one day program software that enables the invention of the self-driving cars</p>
+          </div>
+          <div class="card">
+            <h3>Social Media</h3>
+            <a href="https://github.com/natebrunnett">Github icon natebrunnett</a>
+          </div>
+        </div>
+
       </div>
 
-      <div class="rightcolumn">
-        <div class="card">
-          <h2>About Me</h2>
-          <img src={pfp} alt="profile picture" />
-          <p>I hope to one day program software that enables the invention of the self-driving cars</p>
-        </div>
-        <div class="card">
-          <h3>Follow Me</h3>
-          <a href="https://github.com/natebrunnett">Github:natebrunnett</a>
-        </div>
-      </div>
-
     </div>
-
-    <div class="footer">
-      <h2>Business Inquiries</h2>
-      <h5>nathanieljbrunnett@gmail.com</h5>
-    </div>
-
+    
+    <footer>
+        <h2>Business Inquiries</h2>
+        <h5>nathanieljbrunnett@gmail.com</h5>
+    </footer>
+    
     </>
   );
 }
